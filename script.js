d6ca88bcbe87de0f5bbeb7e8bf3881f42467b764
djembeman2 <52840818+djembeman2@users.noreply.github.com>
2023-12-20 08:18:58 -0600

    all the tags are working properly

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
 // First we will use document.querySelectorAll to
 // get all the elements that match our selection.
 // Then we will 
+elms = document.querySelectorAll("h1")
+
 let chocolateElms, cottonElms, SnoopElms, KittenElms, CharmsEmls,
 JumpsElms, CakesElms, ChoolElms, CrazyElms, SlidesElms, BublesElms,
 CabbinElms, CarryElms, BakesElms;
@@ -40,38 +42,52 @@ SlidesElms[9].style.color= "yellow";
 BublesElms=document.querySelectorAll("h1");
 BublesElms[10].style.color= "tan";
 
-CabbinElms=document.querySelectorAll("h1");
-CabbinElms[11].style.color= "blue"
+CabbinElms= document.querySelectorAll("h1");
+CabbinElms[11].style.color= "blue";
 
-CarryElms=document.querySelectorAll("h1");
-CarryElms[12].style.color= "red"
+CarryElms= document.querySelectorAll("h1");
+CarryElms[12].style.color= "red";
 
 BakesElms=document.querySelectorAll("h1");
-BakesElms[13].style.color= "purple"
+BakesElms[13].style.color= "purple";
 
 
 
-let elms;
-elms = document.querySelectorAll("h1")
-console.log(elms)
+for (elms of elms);
+{
+  elms= style.backgroundColor("blue");
+
+}
+
 // Declare the variable to hold our elements.
 
 
 // Select the elements we wish to change.
-
 elms[1].style.color = "red"
-elms[2].style.color = "red"
+
 elms[3].style.color = "red"
 elms[4].style.color = "red"
-elms[5].style.color = "red"
-elms[6].style.color = "red"
+
 elms[7].style.color = "red"
 elms[8].style.color = "red"
 elms[9].style.color = "red"
-elms[10].style.color = "red"
-elms[11].style.color = "red"
+
 elms[12].style.color = "red"
 elms[13].style.color = "red"
+
+
+const x = document.querySelectorAll("p");
+x[0].style.backgroundColor= "green";
+x[1].style.backgroundColor="red";
+x[2].style.backgroundColor="orange";
+x[3].style.backgroundColor="blue";
+x[4].style.backgroundColor="black";
+x[5].style.backgroundColor="purple";
+x[6].style.backgroundColor="pink";
+x[7].style.backgroundColor="yellow";
+x[8].style.backgroundColor="lightgreen";
+x[9].style.backgroundColor="black";
+
 
 // Loop through the elements and change them.
 function changeElms(){
@@ -79,9 +95,13 @@ function changeElms(){
     window.alert("BakesElms");
     BakesElms = document.querySelector("#Bakes-h1");
     BakesElms.style.color = 'purple';
+  
   }
-  coolbutton = document.querySelector("#Lynndale-btn");
-  coolbutton.onclick = changeElms;
+
+
+
+  coolbutton = document.querySelectorAll("#Lynndale-btn");
+  coolbutton.onclick = changeElmsofElms;
   
 
 
@@ -93,13 +113,26 @@ function changeElms(){
 
 // TODO: select and change the span tags
 //       then loop through the elements and change them.
+const y= document.querySelectorAll("span");
+y[0] .style.backgroundColor="pink";
+y[1].style.backgroundColor="black";
+y[2].style.backgroundColor="white";
+y[3].style.backgroundColor="red";
+y[4].style.backgroundColor="white";
+y[5].style.backgroundColor="yellow";
+y[6].style.backgroundColor="black";
+y[7].style.backgroundColor="blue";
+y[8].style.backgroundColor="white";
+y[9].style.backgroundColor="red";
+
 
 
 // TODO: Go back to index.html and add
 //       a class called "jump" to 10 elements.
 //       These can be any elements you want.
 //       For example: 2 h1 tags, 3 p tags, 5 span tags.
-
+const f= document.querySelectorAll("jump");
+f[0].style.backgroundColor="yellow";
 
 // TODO: select and change the elements with a .jump class 
 // Set the style of each jump so that the element moves up 10px on the page.
